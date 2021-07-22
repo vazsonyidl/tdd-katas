a46147983a0dd63a91797ac782ed17da711fc737
danielvazsonyi <daniel.vazsonyi@balabit.com>
2021-07-22 18:34:02 +0200

    init

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/**
+ * @copyright One Identity 2021
+ * @license All Rights Reserved
+ */
+
+const esModules = ['validator'].join('|');
+
+module.exports = {
+  moduleDirectories: ['node_modules'],
+  rootDir: '.',
+  testMatch: ['**/+(*.)+(spec).+(ts|js)?(x)'],
+  transformIgnorePatterns: [`./node_modules/(?!${esModules})`],
+  moduleFileExtensions: ['ts', 'js', 'html'],
+};
---
+++ wallaby.js
@@ -0,0 +1,6 @@
+module.exports = () => ({
+  autoDetect: true,
+  testFramework: {
+    configFile: './jest.config.js',
+  },
+});
