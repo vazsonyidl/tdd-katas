edcda4fceaeb2eff65d1a67a032a46aa0fe46bb6
vazsonyidl <danielvazsonyi@gmail.com>
2022-04-04 18:12:10 +0300

    two-sum - improve performance

--- leetcode/easy/js/two-sum.js
+++ leetcode/easy/js/two-sum.js
@@ -22,3 +22,39 @@ const twoSum = (nums, target) => {
  * @space the space complexity is O(1), because is does not depends on the
  * length of the input array
 */
+
+// Reduce runtime complexity:
+
+/**
+ * @idea we need to keep a Mapping between the element and its index
+ * What data structure has the best lookup time for this? - hashmap
+ * Loop over the input array first, store each element in a Map where
+ * each <key-value> pair is a <number-index> pair. 
+ * On the second loop, we check if the target-number exists in the hash-table.
+ * If yes, return the two indicies. If not, continue looping
+ */
+
+ const twoSumWithMap = (nums, target) => {
+    const hashmap = new Map();
+
+    for(let i= 0; i<nums.length; i++) {
+        hashmap.set(nums[i], i);
+    }
+
+    for(let i = 0; i<nums.length; i++) {
+        if(hashmap.has(target - nums[i]) && i !== hashmap.get(target - nums[i])) return [i, hashmap.get(target - nums[i])]
+    }
+}
+
+const twoSumWithObject = (nums, target) => {
+    const hashmap = {};
+
+    for(let i= 0; i<nums.length; i++) {
+        hashmap[nums[i]] = i;
+    }
+
+    for(let i = 0; i<nums.length; i++) {
+        if(hashmap[target - nums[i]] && i !== hashmap[target - nums[i]]) 
+            return [i, hashmap[target - nums[i]]]
+    }
+}
